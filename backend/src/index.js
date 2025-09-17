d8136b4431e7f759c1426f6f9616ea0bf6e864ff
hirushan666 <hirushanww@gmail.com>
2025-09-17 08:57:58 +0530

    Dockerize project

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,6 +25,6 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.error("❌ MongoDB connection error:", err));
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log("✅ MongoDB connected"))
+//   .catch(err => console.error("❌ MongoDB connection error:", err));
